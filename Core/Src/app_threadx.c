e13afc6921dcad5e7fbea8d8fb10d8154e6f3cc6
Marcin Sławicz <marcin.slawicz@gmail.com>
2024-09-29 15:25:59 +0200

    game controller thread added

--- Core/Src/app_threadx.c
+++ Core/Src/app_threadx.c
@@ -23,7 +23,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "main.h"
+#include "ux_api.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -33,7 +34,8 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define TX_GAME_CTRL_STACK_SIZE  512
+#define TX_GAME_CTRL_THREAD_PRIO 10
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -43,12 +45,12 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN PV */
-
+TX_THREAD tx_gameController_thread;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN PFP */
-
+static void gameControllerEntry(ULONG thread_input);
 /* USER CODE END PFP */
 
 /**
@@ -61,8 +63,26 @@ UINT App_ThreadX_Init(VOID *memory_ptr)
   UINT ret = TX_SUCCESS;
   /* USER CODE BEGIN App_ThreadX_MEM_POOL */
 
+  TX_BYTE_POOL *byte_pool = (TX_BYTE_POOL*)memory_ptr;
+
+  /* allocate the stack for game controller thread */
+  CHAR* pointer;
+  if(tx_byte_allocate(byte_pool, (VOID**) &pointer, TX_GAME_CTRL_STACK_SIZE, TX_NO_WAIT) != TX_SUCCESS)
+  {
+    return TX_POOL_ERROR;
+  }
+
+
   /* USER CODE END App_ThreadX_MEM_POOL */
   /* USER CODE BEGIN App_ThreadX_Init */
+
+  /* Create game controller thread  */
+  if (tx_thread_create(&tx_gameController_thread, "Game Controller Thread", gameControllerEntry, 0, pointer,
+                       TX_GAME_CTRL_STACK_SIZE, TX_GAME_CTRL_THREAD_PRIO, TX_GAME_CTRL_THREAD_PRIO,
+                       TX_NO_TIME_SLICE, TX_AUTO_START) != TX_SUCCESS)
+  {
+    return TX_THREAD_ERROR;
+  }
   /* USER CODE END App_ThreadX_Init */
 
   return ret;
@@ -87,5 +107,26 @@ void MX_ThreadX_Init(void)
 }
 
 /* USER CODE BEGIN 1 */
+/**
+  * @brief  Function implementing the Game Controller entry thread.
+  * @param  thread_input: Hardcoded to 0.
+  * @retval None
+  */
+void gameControllerEntry(ULONG thread_input)
+{
+  (void) thread_input;
 
+  /* Infinite loop */
+  while (1)
+  {
+    HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, GPIO_PIN_SET);
+    tx_thread_sleep(UX_MS_TO_TICK_NON_ZERO(50));
+    HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, GPIO_PIN_RESET);
+    tx_thread_sleep(UX_MS_TO_TICK_NON_ZERO(100));
+    HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, GPIO_PIN_SET);
+    tx_thread_sleep(UX_MS_TO_TICK_NON_ZERO(50));
+    HAL_GPIO_WritePin(LED_G_GPIO_Port, LED_G_Pin, GPIO_PIN_RESET);
+    tx_thread_sleep(UX_MS_TO_TICK_NON_ZERO(800));            
+  }
+}
 /* USER CODE END 1 */
